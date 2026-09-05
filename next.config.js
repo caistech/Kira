8f5aa103d1a2ca6f4d665a24594234e1f85e34c4
Dennis McMahon <mcmdennis@gmail.com>
2026-09-05 13:33:15 +0800

    Add redirect for /admin/invitations

--- next.config.js
+++ next.config.js
@@ -17,3 +17,14 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+// Redirect for admin/invitations
+module.exports.redirects = async () => {
+  return [
+    {
+      source: '/admin/invitations',
+      destination: '/admin/beta-testers',
+      permanent: true,
+    },
+  ];
+};
